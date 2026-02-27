80ec2a493d4148a6d95a0965e1a6ed587e4715f7
Daniel <dariasmejia24@gmail.com>
2026-02-26 19:56:41 -0500

    Se crea repositorio para proyecto(Repo del Back)

---
+++ FullStack-26-Back/src/main/java/com/example/FullStack_26_Back/FullStack26BackApplication.java
@@ -0,0 +1,13 @@
+package com.example.FullStack_26_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStack26BackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FullStack26BackApplication.class, args);
+	}
+
+}
---
+++ FullStack-26-Back/src/test/java/com/example/FullStack_26_Back/FullStack26BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FullStack_26_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStack26BackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
